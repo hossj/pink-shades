240e6b58ff7adad34ae643f3a8160dd913896a6b
Farbod Ghasemi <farbod.ghasemi@edoramedia.com>
2026-08-14 09:43:19 +0400

    feat: add site content data and local image assets

---
+++ content/content.ts
@@ -0,0 +1,123 @@
+export const media = {
+  hero: "/images/hero-plantation-shutters.jpg",
+  rollerShades: "/images/roller-shades.jpg",
+  commercial: "/images/commercial.jpg",
+};
+
+export const stats = [
+  {
+    id: "years",
+    valueKey: "stats.yearsValue",
+    labelKey: "stats.yearsLabel",
+    highlight: false,
+  },
+  {
+    id: "rating",
+    valueKey: "stats.ratingValue",
+    labelKey: "stats.ratingLabel",
+    highlight: true,
+  },
+  {
+    id: "local",
+    valueKey: "stats.localValue",
+    labelKey: "stats.localLabel",
+    highlight: false,
+  },
+] as const;
+
+export const products = [
+  {
+    id: "woodBlinds",
+    image: "/images/wood-blinds.jpg",
+    nameKey: "products.woodBlinds.name",
+    tagKey: "products.woodBlinds.tag",
+    descriptionKey: "products.woodBlinds.description",
+  },
+  {
+    id: "plantationShutters",
+    image: "/images/hero-plantation-shutters.jpg",
+    nameKey: "products.plantationShutters.name",
+    tagKey: "products.plantationShutters.tag",
+    descriptionKey: "products.plantationShutters.description",
+  },
+  {
+    id: "solarShades",
+    image: "/images/roller-shades.jpg",
+    nameKey: "products.solarShades.name",
+    tagKey: "products.solarShades.tag",
+    descriptionKey: "products.solarShades.description",
+  },
+  {
+    id: "cellularShades",
+    image: "/images/cellular-shades.jpg",
+    nameKey: "products.cellularShades.name",
+    tagKey: "products.cellularShades.tag",
+    descriptionKey: "products.cellularShades.description",
+  },
+] as const;
+
+export const motorizationOptions = [
+  {
+    id: "hardwired",
+    numberKey: "motorization.hardwired.number",
+    titleKey: "motorization.hardwired.title",
+    descriptionKey: "motorization.hardwired.description",
+  },
+  {
+    id: "battery",
+    numberKey: "motorization.battery.number",
+    titleKey: "motorization.battery.title",
+    descriptionKey: "motorization.battery.description",
+  },
+] as const;
+
+export const benefits = [
+  {
+    id: "local",
+    icon: "home",
+    tagKey: "benefits.local.tag",
+    titleKey: "benefits.local.title",
+    descriptionKey: "benefits.local.description",
+  },
+  {
+    id: "estimate",
+    icon: "ruler",
+    tagKey: "benefits.estimate.tag",
+    titleKey: "benefits.estimate.title",
+    descriptionKey: "benefits.estimate.description",
+  },
+  {
+    id: "delivery",
+    icon: "delivery",
+    tagKey: "benefits.delivery.tag",
+    titleKey: "benefits.delivery.title",
+    descriptionKey: "benefits.delivery.description",
+  },
+] as const;
+
+export const testimonials = [
+  {
+    id: "garnet",
+    nameKey: "testimonials.garnet.name",
+    quoteKey: "testimonials.garnet.quote",
+  },
+  {
+    id: "ramin",
+    nameKey: "testimonials.ramin.name",
+    quoteKey: "testimonials.ramin.quote",
+  },
+  {
+    id: "susan",
+    nameKey: "testimonials.susan.name",
+    quoteKey: "testimonials.susan.quote",
+  },
+] as const;
+
+export const estimateInterests = [
+  "estimate.interests.motorized",
+  "estimate.interests.shutters",
+  "estimate.interests.woodBlinds",
+  "estimate.interests.solarShades",
+  "estimate.interests.cellularShades",
+  "estimate.interests.other",
+] as const;
---
+++ content/site.ts
@@ -0,0 +1,50 @@
+export const site = {
+  phoneHref: "tel:+1-703-346-8533",
+  tollFreeHref: "tel:+1-833-746-5636",
+  emailHref: "mailto:info@pinkblindsandshutters.com",
+};
+
+export const navLinks = [
+  { href: "#products", labelKey: "nav.products" },
+  { href: "#motorization", labelKey: "nav.motorization" },
+  { href: "#gallery", labelKey: "nav.gallery" },
+  { href: "#reviews", labelKey: "nav.reviews" },
+] as const;
+
+export const footerColumns = [
+  {
+    id: "company",
+    titleKey: "footer.company",
+    links: [
+      { href: "#", labelKey: "footer.aboutUs" },
+      { href: "#gallery", labelKey: "footer.gallery" },
+      { href: "#", labelKey: "footer.pricing" },
+      { href: "#", labelKey: "footer.news" },
+    ],
+  },
+  {
+    id: "partnerships",
+    titleKey: "footer.partnerships",
+    links: [
+      { href: "#", labelKey: "footer.franchise" },
+      { href: "#", labelKey: "footer.designers" },
+      { href: "#", labelKey: "footer.builders" },
+      { href: "#", labelKey: "footer.realtors" },
+    ],
+  },
+] as const;
+
+export const socialLinks = [
+  {
+    href: "https://facebook.com/pinkblindsandshutters",
+    labelKey: "footer.facebook",
+  },
+  {
+    href: "https://instagram.com/pinkblindsandshutters",
+    labelKey: "footer.instagram",
+  },
+  {
+    href: "https://youtube.com/pinkblindsandshutters",
+    labelKey: "footer.youtube",
+  },
+] as const;
